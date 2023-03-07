10c632d7a11ad3ca31b9b2bfd03d55227223fd81
masudra <masudrana6278312@gmail.com>
2023-03-07 22:50:40 +0600

    2nd commit

---
+++ login.js
@@ -0,0 +1,135 @@
+document.getElementById('submit-btn') .addEventListener('click',function(){
+    const userEmail =document.getElementById('emaile-adrres');
+    const email =userEmail.value ;
+ 
+    const userPassword =document.getElementById('password-input');
+    const password =userPassword.value;
+    // 1  Masud Rana
+    if(email === 'mrbank@gmail.com' && password ==='mrbank123'){
+       window.location.href = 'dashbord.html';
+    }
+    // 2   
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //3  
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    // 4  
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    // 5  
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  6
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+    }
+    // 7
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  8
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  9
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  10
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  11
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  12
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  13
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  14
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  15
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  16
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  17
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  18
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  19
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  20
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  21
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  22
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  23
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  24
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  25
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  26
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  27
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  28
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  29
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+    //  30
+    else if(email === 'mrjubin070@gmail.com' && password ==='mrjubin070@'){
+        window.location.href = 'dashbord.html';
+     }
+   
+    else{
+     alert('Invalide Email and Password')
+    }
+ 
+    
+ 
+ })
+ 
